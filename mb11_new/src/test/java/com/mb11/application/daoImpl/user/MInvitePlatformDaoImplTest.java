352e8dea9816a73c962888a276844f49de763ac8
Ravi Yadav <ravi.yadav@icreon.com>
2019-01-14 11:49:45 +0530

    third commit

---
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/MInvitePlatformDaoImplTest.java
@@ -0,0 +1,14 @@
+package com.mb11.application.daoImpl.user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MInvitePlatformDaoImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/UserInvitePlatformLinkDaoImplTest.java
@@ -0,0 +1,14 @@
+package com.mb11.application.daoImpl.user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserInvitePlatformLinkDaoImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
