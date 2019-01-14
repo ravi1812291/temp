3f6e29efad35252d6e1e2f09aadaf79bf2a6e8d0
Ravi Yadav <ravi.yadav@icreon.com>
2019-01-14 13:15:29 +0530

    third commit

--- mb11_new/src/test/java/com/mb11/application/daoImpl/user/RolesUserLinkDaoImplTest.java
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/RolesUserLinkDaoImplTest.java
@@ -2,13 +2,65 @@ package com.mb11.application.daoImpl.user;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import javax.transaction.Transactional;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mb11.application.model.user.RoleUserLink;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RolesUserLinkDaoImplTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	@Autowired
+	private RoleUserLinkDaoImpl rui;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+//	
+//	@Test
+//	@Transactional
+//	public void addRolesuserLink()
+//	{
+//		rui.addRoleUserLink(new RoleUserLink());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getAllRolesuserLink()
+//	{
+//		rui.getAllRoleUserLink();
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getRolesuserLinkById()
+//	{
+//		rui.getRoleUserLinkById(1L);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void removeRolesuserLink()
+//	{
+//		rui.deleteRoleUserLink(1L);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void updateRolesuserLink()
+//	{
+//		rui.updateRoleUserLink(new RoleUserLink());
+//	}
+	
+	
+	
+	
+	
 
 }
