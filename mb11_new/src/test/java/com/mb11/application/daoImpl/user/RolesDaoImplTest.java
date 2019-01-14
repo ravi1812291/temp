4bbeeb83a34c56f1bbe0646ccb0e00aea711a366
Ravi Yadav <ravi.yadav@icreon.com>
2019-01-14 13:09:04 +0530

    third commit

--- mb11_new/src/test/java/com/mb11/application/daoImpl/user/RolesDaoImplTest.java
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/RolesDaoImplTest.java
@@ -2,6 +2,8 @@ package com.mb11.application.daoImpl.user;
 
 import static org.junit.Assert.*;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mb11.application.model.user.Roles;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,5 +22,45 @@ public class RolesDaoImplTest {
 	private RolesDaoImpl rdi;
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
+//	@Test
+//	@Transactional
+//	public void addRoles()
+//	{
+//		rdi.addRoles(new Roles());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getAllRoles()
+//	{
+//		rdi.getAllRoles();
+//	}
+//	
+//
+//	@Test
+//	@Transactional
+//	public void getRolesById()
+//	{
+//		rdi.getRolesById(1L);
+//	}
+//	
+//
+//	@Test
+//	@Transactional
+//	public void updateRoles()
+//	{
+//		rdi.updateRoles(new Roles());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void removeRoles()
+//	{
+//		rdi.deleteRoles(1l);
+//	}
+	
+	
 
 }
