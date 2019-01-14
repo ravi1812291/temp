d99f574b36ca21e78fad24987ca9c91a8f6b4c78
Ravi Yadav <ravi.yadav@icreon.com>
2019-01-14 13:25:57 +0530

    third commit

--- mb11_new/src/test/java/com/mb11/application/daoImpl/user/MInvitePlatformDaoImplTest.java
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/MInvitePlatformDaoImplTest.java
@@ -2,13 +2,65 @@ package com.mb11.application.daoImpl.user;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import javax.transaction.Transactional;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mb11.application.model.user.MInvitePlatform;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MInvitePlatformDaoImplTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	@Autowired
+	private MInvitePlatformDaoImpl mi;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+//	@Test
+//	@Transactional
+//	public void addMInvitePlatform()
+//	{
+//		mi.addMInvitePlatform(new MInvitePlatform());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getAllMInvitePlatform()
+//	{
+//		mi.getAllMInvitePlatform();
+//	}
+//	
+//
+//	@Test
+//	@Transactional
+//	public void getMInvitePlatformById()
+//	{
+//		mi.getMInvitePlatformById(1L);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void updateMInvitePlatform()
+//	{
+//		mi.updateMInvitePlatform(new MInvitePlatform());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void removeMInvitePlatform()
+//	{
+//		mi.deleteMInvitePlatform(1l);
+//	}
+//	
+//	
+//	
+	
 
 }
--- mb11_new/src/test/java/com/mb11/application/daoImpl/user/UserInvitePlatformLinkDaoImplTest.java
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/UserInvitePlatformLinkDaoImplTest.java
@@ -2,13 +2,61 @@ package com.mb11.application.daoImpl.user;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import javax.transaction.Transactional;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mb11.application.model.user.UserInvitePlatformLink;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserInvitePlatformLinkDaoImplTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	@Autowired
+	private UserInvitePlatformLinkDaoImpl mi;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+//	@Test
+//	@Transactional
+//	public void addUserInvitePlatformLink()
+//	{
+//		mi.addUserInvitePlatformLink(new UserInvitePlatformLink());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void updateUserInvitePlatformLink()
+//	{
+//		mi.updateUserInvitePlatformLink(new UserInvitePlatformLink());
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getAllUserInvitePlatformLink()
+//	{
+//		mi.getAllUserInvitePlatformLink();
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getUserInvitePlatformLinkById()
+//	{
+//		mi.getUserInvitePlatformLinkById(1L);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void removeUserInvitePlatformLink()
+//	{
+//		mi.deleteUserInvitePlatformLink(1l);
+//	}
+	
 
 }
