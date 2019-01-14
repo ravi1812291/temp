e71f98b71eafc02d9a2670c89d0748d0c0afdee1
Ravi Yadav <ravi.yadav@icreon.com>
2019-01-14 13:03:32 +0530

    third commit

--- mb11_new/src/test/java/com/mb11/application/daoImpl/user/AddressDaoImplTest.java
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/AddressDaoImplTest.java
@@ -2,6 +2,8 @@ package com.mb11.application.daoImpl.user;
 
 import static org.junit.Assert.*;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -23,10 +25,44 @@ public class AddressDaoImplTest {
 	
 	
 //@Test	
+//@Transactional
 //public void addAddress()
 //{
 //	adi.addAddress(new Address("test1","test","test","test","test","test"));
 //}
+	
+//	@Test
+//	@Transactional
+//	public void getAllAddress()
+//	{
+//	adi.getAllAddress();	
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getAddressById()
+//	{
+//	adi.getAddressById(1L);
+//	}
+//	
+//
+//	@Test
+//	@Transactional
+//	public void removeAddress()
+//	{
+//	adi.deleteAddress(1l);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void updateAddress()
+//	{
+//	adi.updateAddress(new Address());
+//	}
+	
+	
+	
+	
 	
 	
 
--- mb11_new/src/test/java/com/mb11/application/daoImpl/user/UsersDaoImplTest.java
+++ mb11_new/src/test/java/com/mb11/application/daoImpl/user/UsersDaoImplTest.java
@@ -2,6 +2,8 @@ package com.mb11.application.daoImpl.user;
 
 import static org.junit.Assert.*;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mb11.application.model.user.Users;
 
 
 @RunWith(SpringRunner.class)
@@ -19,5 +23,44 @@ public class UsersDaoImplTest {
 	private UsersDaoImpl udi;
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+//	@Test
+//	@Transactional
+//	public void addUsers()
+//	{
+//		udi.addUsers(new Users());	
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getAllUsers()
+//	{
+//		udi.getAllUsers();	
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void getUsersById()
+//	{
+//		udi.getUsersById(1L);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void removeUsers()
+//	{
+//		udi.deleteUsers(1l);
+//	}
+//	
+//	@Test
+//	@Transactional
+//	public void updateUsers()
+//	{
+//		udi.updateUsers(new Users());
+//	}
+//	
+	
+	
+	
 	
 }
